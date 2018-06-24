89f78a12f0c61b3877b6f89ebb20bf9dc77aa0a8
t005 <t005@mail.nksh.tp.edu.tw>
2018-06-24 05:09:47 -0400

    update

--- account/views.py
+++ account/views.py
@@ -51,15 +51,32 @@ def homepage(request):
     teacher = User.objects.filter(groups__name='teacher').count()
     student = Enroll.objects.values('student_id').distinct().count()
     work = Work.objects.all().count()
-    class_scratch = Classroom.objects.filter(lesson=1).count()
-    class_vphysics =  Classroom.objects.filter(lesson=2).count()
-    class_euler =  Classroom.objects.filter(lesson=3).count()
+    class_scratch_pool = [classroom for classroom in Classroom.objects.filter(lesson=1)]    
+    class_scratch_ids = map(lambda a: a.id, class_scratch_pool)    
+    class_scratch = len(class_scratch_pool)    
+    class_vphysics_pool = [classroom for classroom in Classroom.objects.filter(lesson=2)]    
+    class_vphysics_ids = map(lambda a: a.id, class_vphysics_pool)    
+    class_vphysics = len(class_vphysics_pool)    
+    class_euler_pool = [classroom for classroom in Classroom.objects.filter(lesson=3)]    
+    class_euler_ids = map(lambda a: a.id, class_euler_pool)    
+    class_euler = len(class_euler_pool)      
     work_scratch = Work.objects.filter(lesson_id=1).count()
     work_vphysics = Work.objects.filter(lesson_id=2).count()
     work_euler = Work.objects.filter(lesson_id=3).count()
     works = [work, [class_scratch, work_scratch], [class_vphysics, work_vphysics], [class_euler, work_euler]]
-            
-    return render_to_response('homepage.html', {'works':works, 'teacher':teacher, 'student':student, 'classroom_count':classroom_count, 'row_count':row_count, 'user_count':len(users), 'admin_profile': admin_profile}, context_instance=RequestContext(request))
+    certificate_scratch1 = Enroll.objects.filter(certificate1=True).count() 
+    certificate_scratch2 = Enroll.objects.filter(certificate2=True).count() 
+    certificate_scratch3 = Enroll.objects.filter(certificate3=True).count()
+    certificate_scratch4 = Enroll.objects.filter(certificate4=True).count() 
+    certificate_scratch = certificate_scratch1+certificate_scratch2+certificate_scratch3+certificate_scratch4
+    certificate_vphysics = Enroll.objects.filter(certificate_vphysics=True).count()
+    certificate_euler = Enroll.objects.filter(certificate_euler=True).count()
+    enroll_scratch = Enroll.objects.filter(classroom_id__in=class_scratch_ids, seat__gt=0).count()
+    enroll_vphhysics = Enroll.objects.filter(classroom_id__in=class_vphysics_ids, seat__gt=0).count()
+    enroll_euler = Enroll.objects.filter(classroom_id__in=class_euler_ids, seat__gt=0).count()
+    certificate = certificate_scratch+certificate_vphysics+certificate_euler
+    certificates = [certificate, [enroll_scratch, certificate_scratch], [enroll_vphhysics, certificate_vphysics], [enroll_euler, certificate_euler]]
+    return render_to_response('homepage.html', {'certificates':certificates, 'works':works, 'teacher':teacher, 'student':student, 'classroom_count':classroom_count, 'row_count':row_count, 'user_count':len(users), 'admin_profile': admin_profile}, context_instance=RequestContext(request))
   
 # 網站大廳
 def dashboard(request):
