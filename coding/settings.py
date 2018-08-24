c6f180a496272a966d686bb288cb0a834fdee5d2
t005 <t005@mail.nksh.tp.edu.tw>
2018-08-24 06:16:02 -0400

    update

--- coding/settings.py
+++ coding/settings.py
@@ -135,7 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = "photologue/photo/"
-MEDIA_URL = "/"
+MEDIA_URL = "/photologue/"
 
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, "static"),
