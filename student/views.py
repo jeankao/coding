214b5a94bc7591eb0ec2eb36e6211f80d620bd29
Jean, Hue-Ching Kao <koma@mail.nksh.tp.edu.tw>
2019-01-20 05:54:49 +0800

    update

--- student/views.py
+++ student/views.py
@@ -781,7 +781,10 @@ class WorkListView(ListView):
             end = work_pool[len(work_pool)-1].publication_date
         else:
             start = datetime.today()
-            end = datetime.today()            
+            end = datetime.today()   
+        timezone = pytz.timezone("Asia/Taipei")
+        start = timezone.localize(start)
+        end = timezone.localize(end)                     
         queryset = []
         daterange = [start + timedelta(days=x) for x in range(0, (end-start).days+1)]
         for day in reversed(daterange):
